7cc47fafd28762c1a5a486da8e2fd94919f088ce
Erik Strottmann <erik.strottmann@gmail.com>
2014-07-11 05:47:52 -0600

    Add demo Xcode project

---
+++ ESSPhoneNumberFieldDemo/ESSPhoneNumberFieldDemo/ESSPhoneNumberFieldDemo/ESSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ESSAppDelegate.h
+//  ESSPhoneNumberFieldDemo
+//
+//  Created by Erik Strottmann on 7/11/14.
+//  Copyright (c) 2014 Erik Strottmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ESSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ESSPhoneNumberFieldDemo/ESSPhoneNumberFieldDemo/ESSPhoneNumberFieldDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ESSPhoneNumberFieldDemo
+//
+//  Created by Erik Strottmann on 7/11/14.
+//  Copyright (c) 2014 Erik Strottmann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ESSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ESSAppDelegate class]));
+    }
+}
